22e5afc0db065cdeb7be8e796971df754536ce63
Victor <chenvictor247@gmail.com>
2021-11-27 23:06:27 -0500

    Clean up, updating model

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing import image
@@ -6,8 +7,7 @@ import numpy as np
 app = Flask(__name__)
 
 dic = {0 : 'Has COVID', 1 : "Does Not Have COVID"}
-
-model = load_model('model_V2.h5')
+model = load_model('model_V10.h5')
 
 model.make_predict_function()
 
@@ -33,13 +33,16 @@ def about():
     return render_template('about.html')
 
 @app.route('/submit', methods=['GET', 'POST'])
-def get_hours():
+def submit():
     if request.method == 'POST':
         img = request.files['my_image']
         img_path = './static/' + img.filename
         img.save(img_path)
         p = predict_label(img_path)
-    return render_template('index.html', prediction = p, img_path = img_path)
-
-if __name__ == '__main__':
+    
+        return render_template('index.html', prediction = p, img_path = img_path)
+    else: 
+        return render_template('index.html')
+if __name__ == "__main__":
     app.run(debug = True)
+#app.run(threaded=True, port = int(os.environ.get('PORT', 5000)))
